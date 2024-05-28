68cced7eef21336bf7492bbabb37bf8a0934f573
mikan48 <mukanshin.san@gmail.com>
2024-05-28 12:01:59 +0300

    shared_ptr + weak_ptr update

--- sandbox/sandbox/shared_ptr.h
+++ sandbox/sandbox/shared_ptr.h
@@ -8,6 +8,29 @@ private:
     T* m_ptr = nullptr;
     size_t* m_count = nullptr;
 
+    template <typename U>
+    struct ControlBlock {
+        size_t shared_count;
+        size_t weak_count;
+        U object;
+    };
+
+    template <typename U>
+    class weak_ptr;
+
+    ControlBlock<T>* m_cptr = nullptr;
+
+    template <typename U, typename... Args>
+    friend shared_ptr<U> make_shared(Args&&... args);
+
+    struct make_shared_t;
+
+    template <typename... Args>
+    shared_ptr(make_shared_t, ControlBlock<T>* storage_ptr, Args&&... args)
+        : m_cptr(storage_ptr)
+    {
+    }
+
 public:
     shared_ptr() { }
 
@@ -49,20 +72,54 @@ public:
 
     ~shared_ptr()
     {
-        if (*m_count > 1) {
-            --(*m_count);
+        --m_cptr->shared_count;
+
+        if (m_cptr->shared_count > 0) {
             return;
         }
-        delete m_ptr;
-        delete m_count;
+
+        if (m_cptr->weak_count == 0) {
+            delete cptr;
+            return;
+        }
+
+        cptr->object.~T();
+    }
+};
+
+template <typename T>
+class weak_ptr {
+    ControlBlock<T>* m_cptr = nullptr;
+
+public:
+    weak_ptr(const shared_ptr<T>& ptr)
+        : m_cptr(ptr.m_cptr)
+    {
+    }
+
+    bool expired() const
+    {
+        return m_cptr->shared_count > 0;
+    }
+
+    shared_ptr<T> lock() const
+    {
+    }
+
+    ~weak_ptr()
+    {
+        
     }
 };
 
 template <typename T, typename... Args>
 shared_ptr<T> make_shared(Args&... args)
 {
-    auto ptr = new T(std::forward<Args>(args));
-    return shared_ptr<T>(ptr);
+    auto ptr = new ControlBlock<T> { 1, T(std::forward<Args>(args)...) };
+    return shared_ptr<T>(shared_ptr::make_shared_t(), ptr);
+
+    // auto ptr = new T(std::forward<Args>(args));
+    // return shared_ptr<T>(ptr);
 }
 
 }
